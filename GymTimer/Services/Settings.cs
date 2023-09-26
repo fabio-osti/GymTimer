399b422cf2e4278bb0ef24f6adb7ae5a0f863b3d
Fabio Osti <fabio.osti@hotmail.com>
2023-09-26 16:26:51 -0300

    Settings cache

--- GymTimer/Services/Settings.cs
+++ GymTimer/Services/Settings.cs
@@ -5,14 +5,31 @@ namespace GymTimer.Services;
 public sealed partial class Settings : ObservableObject
 {
     [ObservableProperty]
-    private bool _autoStartSet = true;
+    private bool _autoStartSet = Preferences.Default.Get(nameof(AutoStartSet), true);
+    partial void OnAutoStartSetChanged(bool value)
+    {
+        Preferences.Default.Set(nameof(AutoStartSet), value);
+    }
 
     [ObservableProperty]
-    private bool _playSounds = true;
+    private bool _playSounds = Preferences.Default.Get(nameof(PlaySounds), true);
+    partial void OnPlaySoundsChanged(bool value)
+    {
+        Preferences.Default.Set(nameof(PlaySounds), value);
+    }
 
     [ObservableProperty]
-    private bool _showNotification = true;
+    private bool _showNotification = Preferences.Default.Get(nameof(ShowNotification), true);
+    partial void OnShowNotificationChanged(bool value)
+    {
+        Preferences.Default.Set(nameof(ShowNotification), value);
+    }
 
     [ObservableProperty]
-    private int _runningOutThreshold = 3;
+    private int _runningOutThreshold = Preferences.Default.Get(nameof(RunningOutThreshold), 3);
+
+    partial void OnRunningOutThresholdChanged(int value)
+    {
+        Preferences.Default.Set(nameof(RunningOutThreshold), value);
+    }
 }
